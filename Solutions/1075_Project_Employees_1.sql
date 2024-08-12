49b07b36a2f8e2ac7e03f4f2c001f7e0136c2b87
Sharvil21 <sshastri2102@gmail.com>
2024-08-12 12:15:51 -0400

    Added the Solution

--- Solutions/1075_Project_Employees_1.sql
+++ Solutions/1075_Project_Employees_1.sql
@@ -1,1 +1,7 @@
-
+-- Write your PostgreSQL query statement below
+SELECT
+project_id, ROUND(AVG(1.0*experience_years),2) AS average_years
+FROM Project
+JOIN Employee
+USING(employee_id)
+GROUP BY 1
