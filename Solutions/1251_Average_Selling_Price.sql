57f5ecb5979f3ebdd00f62364ee176b849f451f4
Sharvil21 <sshastri2102@gmail.com>
2024-08-12 12:12:11 -0400

    Added the Solution

--- Solutions/1251_Average_Selling_Price.sql
+++ Solutions/1251_Average_Selling_Price.sql
@@ -1,1 +1,8 @@
-
+-- Write your PostgreSQL query statement below
+SELECT
+a.product_id, CASE WHEN SUM(units) IS NOT NULL THEN ROUND(1.0*SUM(price*units)/SUM(units),2) ELSE 0 END AS average_price
+FROM Prices a
+LEFT JOIN UnitsSold b
+ON a.product_id = b.product_id
+AND b.purchase_date BETWEEN a.start_date AND a.end_date
+GROUP BY 1
