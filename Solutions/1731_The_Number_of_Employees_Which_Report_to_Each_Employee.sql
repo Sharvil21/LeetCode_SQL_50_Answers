db8c34c4316e4a44f8dfd66d9f602ae5616bcdf8
Sharvil21 <sshastri2102@gmail.com>
2024-08-14 16:39:23 -0400

    Created the .sql file

--- Solutions/1731_The_Number_of_Employees_Which_Report_to_Each_Employee.sql
+++ Solutions/1731_The_Number_of_Employees_Which_Report_to_Each_Employee.sql
@@ -1,1 +1,8 @@
---- Created the file
+-- Write your PostgreSQL query statement below
+SELECT
+a.employee_id, a.name, COUNT(COALESCE(a.reports_to,0)) AS reports_count, ROUND(AVG(b.age),0) AS average_age
+FROM Employees a
+JOIN Employees b
+ON a.employee_id = b.reports_to
+GROUP BY 1,2
+ORDER BY 1;
