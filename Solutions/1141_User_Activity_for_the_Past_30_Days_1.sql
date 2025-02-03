1ae6665e0a10c02dcb91e0b5d2e5c9324e8aa9e9
Sharvil21 <sshastri2102@gmail.com>
2025-02-03 10:12:47 -0500

    Added the Solution

--- Solutions/1141_User_Activity_for_the_Past_30_Days_1.sql
+++ Solutions/1141_User_Activity_for_the_Past_30_Days_1.sql
@@ -1,1 +1,6 @@
--- Added this comment for testing
+-- Write your PostgreSQL query statement below
+SELECT
+activity_date AS day, COUNT(DISTINCT user_id) AS active_users
+FROM Activity
+WHERE activity_date <= '2019-07-27' AND '2019-07-27' - activity_date < 30 
+GROUP BY 1
