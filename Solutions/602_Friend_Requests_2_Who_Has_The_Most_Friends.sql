eac690a4e9830ad5ed90f7aecc7865ed3dd46d19
Sharvil21 <sshastri2102@gmail.com>
2025-02-06 12:39:47 -0500

    Added the solution

--- Solutions/602_Friend_Requests_2_Who_Has_The_Most_Friends.sql
+++ Solutions/602_Friend_Requests_2_Who_Has_The_Most_Friends.sql
@@ -1,1 +1,16 @@
---
+-- Write your PostgreSQL query statement below
+WITH cte AS (SELECT
+requester_id, accepter_id
+FROM RequestAccepted
+
+UNION ALL
+
+SELECT accepter_id, requester_id
+FROM RequestAccepted)
+
+SELECT
+requester_id AS id, COUNT(accepter_id) AS num
+FROM cte
+GROUP BY 1
+ORDER BY 2 DESC
+LIMIT 1
